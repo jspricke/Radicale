8b9de5791d4c185b44b6ba25781cbee981764767
Jochen Sprickerhof <jochen@sprickerhof.de>
2013-10-19 23:34:54 +0200

    Implement updates of Abook file

--- radicale/storage/abook.py
+++ radicale/storage/abook.py
@@ -26,6 +26,7 @@ from os.path import getmtime
 from time import gmtime
 from threading import Lock
 from ConfigParser import ConfigParser
+from vobject import readComponents
 
 class Abook(object):
 
@@ -90,11 +91,66 @@ class Abook(object):
         self.update2()
         return gmtime(self._last_modified)
 
+    def abook(self, vcard, section, cParser):
+        cParser.add_section(section)
+        cParser.set(section, 'name', vcard.fn.value.encode('utf-8'))
+
+        if hasattr(vcard, 'email'):
+            email = [e.value for e in vcard.email_list]
+            cParser.set(section, 'email', ','.join(email).encode('utf-8'))
+        if hasattr(vcard, 'note_list'):
+            for (i, note) in enumerate(vcard.note_list):
+                cParser.set(section, 'custom%d' % (i+1), note.value.encode('utf-8'))
+        if hasattr(vcard, 'tel_list'):
+            for tel in vcard.tel_list:
+                if tel.TYPE_param == 'HOME':
+                    cParser.set(section, 'phone', tel.value.encode('utf-8'))
+                elif tel.TYPE_param == 'WORK':
+                    cParser.set(section, 'workphone', tel.value.encode('utf-8'))
+                elif tel.TYPE_param == 'CELL':
+                    cParser.set(section, 'mobile', tel.value.encode('utf-8'))
+        if hasattr(vcard, 'nickname'): cParser.set(section, 'nick', vcard.nickname.value.encode('utf-8'))
+        if hasattr(vcard, 'url'): cParser.set(section, 'url', vcard.url.value.encode('utf-8'))
+        if hasattr(vcard, 'adr'):
+            if vcard.adr.value.street:
+                cParser.set(section, 'address', vcard.adr.value.street.encode('utf-8'))
+            if vcard.adr.value.extended:
+                cParser.set(section, 'address2', vcard.adr.value.extended.encode('utf-8'))
+            if vcard.adr.value.city:
+                cParser.set(section, 'city', vcard.adr.value.city.encode('utf-8'))
+            if vcard.adr.value.region:
+                cParser.set(section, 'state', vcard.adr.value.region.encode('utf-8'))
+            if vcard.adr.value.code and vcard.adr.value.code != '0':
+                cParser.set(section, 'zip', vcard.adr.value.code.encode('utf-8'))
+            if vcard.adr.value.country:
+                cParser.set(section, 'country', vcard.adr.value.country.encode('utf-8'))
+
     def append(self, text):
-        raise NotImplementedError
+        cParser = ConfigParser()
+        cParser.read(self._filename)
+
+        for vcard in readComponents(text):
+            if ';' in vcard.uid.value:
+                (section, oldhash) = vcard.uid.value.split(';')
+            else:
+                section = int(cParser.sections[-1]) + 1
+
+            if cParser.has_section(section):
+                raise NotImplementedError
+
+            self.abook(vcard, section, cParser)
+
+        cParser.write(open(self._filename, 'w'))
 
     def remove(self, name):
-        raise NotImplementedError
+        (section, delhash) = name.split(';')
+        delhash = int(delhash)
+        abook = ConfigParser()
+        abook.read(self._filename)
+        linehash = hash(hash(abook.get(section, 'name')))
+        if linehash == delhash:
+            abook.remove_section(section)
+            abook.write(open(self._filename, 'w'))
 
 if __name__ == '__main__':
     from sys import argv, stdout
